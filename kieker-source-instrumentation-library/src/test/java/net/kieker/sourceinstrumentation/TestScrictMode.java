21bad5871b20b35e1c138a384a43b554c8e543ab
Alex Korn <alex@iotiq.de>
2022-12-23 15:33:09 +0100

    Update and add tests for Android strictMode

--- kieker-source-instrumentation-library/src/test/java/net/kieker/sourceinstrumentation/TestScrictMode.java
+++ kieker-source-instrumentation-library/src/test/java/net/kieker/sourceinstrumentation/TestScrictMode.java
@@ -14,32 +14,45 @@ import net.kieker.sourceinstrumentation.util.TestConstants;
 
 public class TestScrictMode {
    
+   static final String C0_0 = "src/main/java/de/peass/C0_0.java";
+   static final String C0_0_FQN = "public void de.peass.C0_0.method0()";
+   static final String C1_0 = "src/main/java/de/peass/C1_0.java";
+   static final String C1_0_FQN = "public void de.peass.C1_0.method0()";
+   static final String PROJECT = "/project_2_extends/";
 
    @Test
    public void testStrictMode() throws IOException {
       InstrumentationConfiguration configuration = getConfiguration(true);
       
-      String changedSource = executeInstrumentation(configuration);
+      String changedSource = executeInstrumentation(configuration, C1_0, C1_0_FQN);
       MatcherAssert.assertThat(changedSource, Matchers.containsString("android.os.StrictMode.setThreadPolicy(policy);"));
+   }
+
+   @Test
+   public void testStrictModeNoExtends() throws IOException {
+      InstrumentationConfiguration configuration = getConfiguration(true);
+      
+      String changedSource = executeInstrumentation(configuration, C0_0, C0_0_FQN);
+      MatcherAssert.assertThat(changedSource, Matchers.not(Matchers.containsString("android.os.StrictMode.setThreadPolicy(policy);")));
    }
    
    @Test
    public void testNoStrictMode() throws IOException {
       InstrumentationConfiguration configuration = getConfiguration(false);
       
-      String changedSource = executeInstrumentation(configuration);
+      String changedSource = executeInstrumentation(configuration, C0_0, C0_0_FQN);
       MatcherAssert.assertThat(changedSource, Matchers.not(Matchers.containsString("android.os.StrictMode.setThreadPolicy(policy);")));
    }
 
-   private String executeInstrumentation(InstrumentationConfiguration configuration) throws IOException {
+   private String executeInstrumentation(InstrumentationConfiguration configuration, String name, String fqn) throws IOException {
       TestConstants.CURRENT_FOLDER.mkdirs();
 
-      File testFile = SourceInstrumentationTestUtil.copyResource("src/main/java/de/peass/C0_0.java", "/project_2/");
+      File testFile = SourceInstrumentationTestUtil.copyResource(name, PROJECT);
 
       InstrumentKiekerSource instrumenter = new InstrumentKiekerSource(configuration);
       instrumenter.instrument(testFile);
 
-      TestSourceInstrumentation.testFileIsInstrumented(testFile, "public void de.peass.C0_0.method0()", "DurationRecord");
+      TestSourceInstrumentation.testFileIsInstrumented(testFile, fqn, "DurationRecord");
       String changedSource = FileUtils.readFileToString(testFile, StandardCharsets.UTF_8);
       return changedSource;
    }
