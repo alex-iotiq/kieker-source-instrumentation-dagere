cbe79c41c2fb6788c222914f23179fcb0def1f30
Alex Korn <alex@iotiq.de>
2022-12-23 15:30:02 +0100

    Add test files for Android strictMode tests

---
+++ kieker-source-instrumentation-library/src/test/resources/project_2_extends/src/main/java/de/peass/C1_0.java
@@ -0,0 +1,9 @@
+package de.peass;
+
+public class C1_0 extends Application{ 
+ public void method0(){
+   final Application rm = new Application();
+   for (int i = 0; i < 10100; i++) {
+      rm.addSomething();
+   } }
+}
